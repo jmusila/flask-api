b0845a4f136919427b6819cce113bb239f371d46
jmusila <jonathanmusila6@gmail.com>
2023-06-13 11:52:14 +0300

    add app requirements

--- validators.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request, jsonify
-from .database_config import User
-
-def check_if_user_exists(email):
-    user = User.query.filter_by(email = email).first()
-    if user:
-        return jsonify(message = 'Email has already been taken.'), 409
